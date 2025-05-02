a966f834182c44f09baf4fda567a250cad00a2bc
ronik297 <ronik.allen@gmail.com>
2025-05-02 13:44:37 +0530

    added starter files

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import Navigation from "./components/Navigation";
-import Logo from "./components/Logo";
+import Navigation from "./_components/Navigation";
+import Logo from "./_components/Logo";
 
 export const metadata = {
   title: "The Wild Oasis",
