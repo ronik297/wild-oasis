1cdb48779f8939248cc192d7afe19c8e02d66450
ronik297 <ronik.allen@gmail.com>
2025-05-02 15:33:52 +0530

    modified homepage

--- app/pages/home-page.js
+++ app/pages/home-page.js
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import bg from "@/public/bg.png";
+
 export default function Page() {
   return (
     <main className="mt-24">
-      <img src="/bg.png" alt="Mountains and forests with two cabins" />
+      <Image src={bg} fill alt="Mountains and forests with two cabins" />
 
       <div className="relative z-10 text-center">
         <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
