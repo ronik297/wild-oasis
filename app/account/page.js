11e65bd53842aba6b8b11d23e0acda885525b50d
ronik297 <ronik.allen@gmail.com>
2025-05-02 16:47:28 +0530

    added profile and reservations page, added date-fns and heroicons/react

--- app/account/page.js
+++ app/account/page.js
@@ -3,5 +3,9 @@ export const metadata = {
 };
 
 export default function Account() {
-  return <div>Account</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Jonas
+    </h2>
+  );
 }
